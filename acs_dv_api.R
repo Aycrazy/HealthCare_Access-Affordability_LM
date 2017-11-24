b765dc3022feec161e4d8d7040dce4ffb38a4115
aycrazy <andrewyaspan@gmail.com>
2017-11-24 14:52:34 -0600

    got started on bump chart, plenty to fix up

---
+++ acs_dv_api.R
@@ -0,0 +1,80 @@
+library('acs')
+library(tidyverse)
+library(readxl)
+library(RSocrata)
+library(data.table)
+library(jsonlite)
+
+setwd('~/Documents/data_viz/health_insurance_marketplace/')
+
+update.packages("acs", clean=T)
+
+Wisco_Counties_total_pop_2011 = acs.fetch(geography=geo.make(state="WI", county="*"), table.number="B01003", endyear=2011)
+
+missing_county_WI = acs.fetch(geography=geo.make(state="WI", fips="17000"), table.number="B01003", endyear=2011)
+
+missing_county_MI = acs.fetch(geography=geo.make(state="MI", fips="18000"), table.number="B01003", endyear=2011)
+
+missing_county_IN = acs.fetch(geography=geo.make(state="IN", fips="26000"), table.number="B01003", endyear=2011)
+
+missing_county_IL = acs.fetch(geography=geo.make(state="IL", fips="55000"), table.number="B01003", endyear=2011)
+
+
+ui_10<- read_xls('2010 County health Rankings National Data.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2010)
+           
+ui_11<- read_xls('2011 County health Rankings National Data.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2011)
+    
+ui_12<- read_xls('2012 County health Rankings National Data.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2012)
+    
+ui_13<- read_xls('2013CountyHealthRankingsNationalData.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2013)
+    
+ui_14<- read_xls('2014 County health Rankings Data.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2014)
+    
+ui_15<- read_xls('2015 County health Rankings Data.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2015)
+    
+ui_16<- read_xls('2016 County health Rankings Data.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2016)
+    
+ui_17<- read_xls('2017CountyHealthRankingsData.xls', sheet='Ranked Measure Data', skip = 1) %>%
+    select(c('State','County', 'FIPS','% Uninsured')) %>%
+    filter( State %in% c('Indiana','Illinois','Wisconsin','Michigan'))%>%
+    mutate(year = 2017)
+
+uni_all_years <- rbind(ui_10, ui_11, ui_12, ui_13, ui_14, ui_15, ui_16, ui_17)
+
+subset(uni_all_years, !is.na(County)) %>%
+    subset(uni_all_years, !is.na(County)) %>%
+    toJSON() %>%
+    write_lines('~/Documents/uninsured_bump/bump_chart_data.json')
+
+uni_state_all_years <- uni_all_years %>%
+    #mutate('% Uninsured' = gsub("\\%", "",'% Uninsured')) #%>%
+    #mutate('% Uninsured' = as.numeric('% Uninsured')) #%>%
+    rename(Uninsured = '% Uninsured') %>%
+    group_by(State,year) %>%
+    summarise( Uninsured = mean(Uninsured, na.rm = TRUE))
+
+#uni_state_all_years %>%
+#    toJSON() %>%
+#    write_lines('~/Documents/uninsured_bump/bump_chart_data.json')
+
---
+++ uninsured_bump/script.js
@@ -0,0 +1,173 @@
+
+
+// Chart Size Setup
+var margin = { top: 35, right: 0, bottom: 30, left: 70 };
+
+var width = 960 - margin.left - margin.right;
+var height = 800 - margin.top - margin.bottom;
+
+options = {State: "Illinois"}
+//Load in JSON Data
+
+d3.json('bump_chart_data.json', function(d){
+		dataset = d.filter(function(d) {return d.State == options.State; });
+    console.log(dataset);
+    makeBumpChart(dataset);
+ });
+
+function makeBumpChart(data){
+	
+
+	console.log(data);
+
+	data.sort(function(a,b){
+		if(b['year'] != a['year']) {
+      return b['year'] - a['year'];
+    }
+		if(b['% Uninsured'] != a['% Uninsured']){
+			return b['% Uninsured'] - a['% Uninsured'];
+		}
+	})
+
+	
+
+	var pos = 1;
+	data[0].position = pos;
+	for(var i=1; i<data.length; i++) {
+	  // this is a new year, so start over
+	  if(data[i - 1].year != data[i].year) {
+	    pos = 1;
+	  } else {
+	    pos++;
+	  }
+	  data[i].position = pos;
+	}
+
+	data.forEach(function(d) {
+    d['class'] = d['County'].toLowerCase().replace(/ /g, '-').replace(/\./g,'');
+     })
+
+  var chart = d3.select('#chart')
+    .append('svg')
+    .attr('width', width + margin.left + margin.top)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  ///////////////////////
+  // Scales
+  var x = d3.scaleBand()
+      .domain(data.map(function(d) { return d['year']; }).reverse())
+      .rangeRound([25, width - 15]);
+
+  var y = d3.scaleLinear()
+      .domain([d3.min(data, function(d) { return d['position'] }), d3.max(data, function(d) { return d['position']; })])
+      .range([20, height - 30]);
+
+  var size = d3.scaleLinear()
+      .domain(d3.extent(data, function(d) { return d['% Uninsured']; }))
+      .range([3, 10]);
+
+  ///////////////////////
+  // Axis
+  var xAxis = d3.axisBottom(x);
+
+  var yAxis = d3.axisLeft(y);
+
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(-"+ x.bandwidth()/2.0 +"," + height + ")")
+      .call(xAxis);
+
+  chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  ///////////////////////
+  // Lines
+  var counties = d3.map(data, function(d) {
+    return d['County'];
+  }).keys();
+
+  counties.forEach(function(county) {
+    var currData = data.filter(function(d) {
+      if(d['County'] == county) {
+        return d;
+      }
+    });
+
+    var line = d3.line()
+        .x(function(d) { return x(d['year']); })
+        .y(function(d) { return y(d['position']); });
+
+    chart.append("path")
+        .datum(currData)
+        .attr("class", county.toLowerCase().replace(/ /g, '-').replace(/\./g,'') )
+    		.attr("style", "fill:none !important")
+        .style('stroke','#7D818A')
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 2)
+        .attr("stroke-opacity", 0.1)
+        .attr("d", line);
+  });
+
+  ///////////////////////
+  // Nodes
+  var node = chart.append("g")
+    .selectAll("circle")
+    .data(data)
+    .enter().append("circle")
+    .attr("class", "uninsured")
+    .attr("cx", function(d) { return x(d['year']); })
+    .attr("cy", function(d) { return y(d['position']); })
+    .attr('fill', 'blue')
+    // replace spaces with - and remove '.' (from d.c. united)
+    .attr("class", function(d) { return d['County'].toLowerCase().replace(/ /g, '-').replace(/\./g,'') })
+    .attr("r", 3)
+    //.attr("r", function(d) { return size(d['goals_for']) })
+    .attr("stroke-width", .3)
+    .attr('opacity', '0.6');
+
+	///////////////////////
+	  // Tooltips
+	  var tooltip = d3.select("body").append("div")
+	      .attr("class", "tooltip");
+
+	  chart.selectAll("circle")
+	      .on("mouseover", function(d) {
+	        chart.selectAll('.' + d['class'])
+	            .classed('active', true);
+
+	        var tooltip_str = "Uninsured %: " + d['% Uninsured'] +
+	        				"<br/>" + "County: " + d['County'] +
+	                "<br/>" + "Year: " + d['year'] ;
+
+	        tooltip.html(tooltip_str)
+	            .style("visibility", "visible");
+	      })
+	      .on("mousemove", function(d) {
+	        tooltip.style("top", event.pageY - (tooltip.node().clientHeight + 5) + "px")
+	            .style("left", event.pageX - (tooltip.node().clientWidth / 2.0) + "px");
+	      })
+	      .on("mouseout", function(d) {
+	        chart.selectAll('.'+d['class'])
+	            .classed('active', false);
+
+	        tooltip.style("visibility", "hidden");
+	      })
+	      .on('click', function(d) {
+	        chart.selectAll('.' + d['class'])
+	            .classed('click-active', function(d) {
+	              // toggle state
+	              return !d3.select(this).classed('click-active');
+	            });
+	      })
+	  };
+
+function changeState(value){
+  options.state = value;
+  d3.json("bump_chart_data.json", function(d) {  
+    dataset = d.filter(function(d) {return d.State == options.State; });
+    makeBumpChart(dataset);
+});};
