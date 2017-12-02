4fc5a1b6a93ab359c0fe0b1b4bf4d2ee51b02e14
aycrazy <andrewyaspan@gmail.com>
2017-12-02 02:16:10 -0600

    little things trying to fix scatter, tooltip still not working and neither is clicking on bubbles

--- bubble_script.js
+++ bubble_script.js
@@ -1,54 +1,3 @@
-var years = d3.range(2015, 2017 + 1);
-var interval = 3000;
-
-
-var blurStable = '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 5 -7';
-var blurIn = '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -11';
-var blurOut = '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 15 -7';
-
-var margin = { top: 15, right: 15, bottom: 45, left: 55 };
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
-
-
-var svg = d3.select('#scatterHealthBubbleChart')
-    .append('svg')
-    .attr('width', width + margin.left + margin.top*2)
-    .attr('height', height + margin.top*2 + margin.bottom)
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-var x = d3.scaleLinear()
-				.range([0, width]);
-var y = d3.scaleLinear()
-				.range([height, 0]);
-var y_alt = d3.scaleLinear()
-                .range([height, 0]);
-var r = d3.scaleLinear()
-				.range([10, 50]);
-var color = d3.scaleOrdinal()
-    .range(([ "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]));
-
-var xAxis = d3.axisBottom()
-						.scale(x);
-var yAxis = d3.axisLeft()
-						.scale(y);
-var dataset;
-
-var f = d3.format(".2f")
-
-var first_time = true;
-
-var yearLabel = svg.append('text')
-    .attr('class', 'year')
-    .attr('x', width / 2)
-    .attr('y', height / 2)
-    .attr('dy', '.28em')
-    .style('font-size', width / 3)
-    .style('text-anchor', 'middle')
-    .style('font-weight', 'bold')
-    .style('opacity', 0.4)
-    .text('2015');
 
 
 d3.json("scatter_area_data.json", initialize);
@@ -56,7 +5,54 @@ d3.json("scatter_area_data.json", initialize);
 function initialize(error, data,first_time) {
     if (error) { throw error }
 
-  	//could the issue be here?
+    var blurStable = '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 5 -7'
+    var blurIn = '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -11'
+    var blurOut = '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 15 -7'
+
+    var margin = { top: 15, right: 15, bottom: 45, left: 55 };
+    var width = 960 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
+
+
+    var bubble = d3.select('#bubble_chart')
+        .append('svg')
+        .attr('width', width + margin.left + margin.top*2)
+        .attr('height', height + margin.top*2 + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var x = d3.scaleLinear()
+                    .range([0, width]);
+    var y = d3.scaleLinear()
+                    .range([height, 0]);
+
+    var r = d3.scaleLinear()
+                    .range([10, 50]);
+    var color = d3.scaleOrdinal()
+        .range(([ "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]));
+
+    var xAxis = d3.axisBottom()
+                            .scale(x);
+    var yAxis = d3.axisLeft()
+                            .scale(y);
+    var dataset;
+
+    var f = d3.format(".2f")
+
+    var first_time = true;
+
+    var yearLabel = bubble.append('text')
+        .attr('class', 'year')
+        .attr('x', width / 2)
+        .attr('y', height / 2)
+        .attr('dy', '.28em')
+        .style('font-size', width / 3)
+        .style('text-anchor', 'middle')
+        .style('font-weight', 'bold')
+        .style('opacity', 0.4)
+        .text('2015');
+
+  	
     x.domain([200, d3.max(data, function (d) { return d.avg_silver_27 })]).nice()
     y.domain([0.00, 100.00]).nice()
     r.domain([0, 400000])
@@ -85,12 +81,17 @@ function initialize(error, data,first_time) {
 
     dataset = data 
 
-    var uniqueStates = data[0].values.map(function (d) { console.log(d,'unique'); return d.key });
+    var uniqueStates = data[0].values.map(function (d) { return d.key });
 
     // from http://www.visualcinnamon.com/2016/06/fun-data-visualizations-svg-gooey-effect.html
 	  // modified to create a filter for each continent group, which can be individually transitioned
 	  
-	  var filters = svg.append('defs')
+    var years = d3.range(2015, 2017 + 1);
+    var interval = 3000;
+
+
+    
+	  var filters = bubble.append('defs')
 	      .selectAll('filter')
 	      .data(uniqueStates)
 	      .enter()
@@ -115,7 +116,7 @@ function initialize(error, data,first_time) {
 	      .attr('in', 'SourceGraphic')
 	      .attr('in2', 'gooey');
 
-	  svg.append('g')
+	  bubble.append('g')
 	      .attr('class', 'x axis')
 	      .attr('transform', 'translate(' + margin.left + ',' + height + ')')
 	      .call(xAxis)
@@ -126,7 +127,7 @@ function initialize(error, data,first_time) {
 	      .style('font-weight', 'bold')
 	      .text('Average Silver Plan Premium Price Per Month ');
 
-	  svg.append('g')
+	  bubble.append('g')
 	      .attr('class', 'y axis')
 	      .attr('transform','translate('+ margin.left + ',' + 0 + ')')
 	      .call(yAxis)
@@ -146,7 +147,6 @@ function initialize(error, data,first_time) {
 	  d3.select('#explode')
 	  .on('click', function () {
 	      uniqueStates.forEach(function (d) {
-	      	console.log(d,'hello');
 	          if (!exploded.has(d)) {
 	              exploded.add(d)
 	              blurTransition.add(d)
@@ -157,8 +157,8 @@ function initialize(error, data,first_time) {
 	  d3.select('#collapse').on('click', function () {
         uniqueStates.forEach(function (d) {
 
-            svg.selectAll('.scatter_temp')
-                    .remove().exit();
+            //bubble.selectAll('.scatter_temp')
+              //      .remove().exit();
 
             if (exploded.has(d)) {
                 exploded.remove(d)
@@ -170,36 +170,20 @@ function initialize(error, data,first_time) {
       d3.select('#reset')
       .on('click', function () {
 
-        svg.selectAll('.scatter_temp')
-                    .remove().exit();
+        //bubble.selectAll('.scatter_temp')
+        //            .remove().exit();
 
           yearIndex = 0
           year = '2015'
-          console.log(yearIndex,'reset worked');
           first_time=false;
-          var states = svg.selectAll('.state')
-                    .data(data[0].values)
-                    .enter().append('g')
-                    .attr('class', 'state')
 
-
-
-    states.append('circle')
-        .attr('class', 'aggregate')
-        .attr('cx', width/2)
-        .attr('cy', height / 2)
-
-        .style('fill', function (d) { return color(d.key) })
-        .style('opacity','.9')
-        .on('click', function (d) { exploded.add(d.key); blurTransition.add(d.state_name) })
-        .append('title').text(function (d) { return d.key })
 
         update()
 
-      }).classed('bump_temp',true)
+      })
 
 
-    var states = svg.selectAll('.state')
+    var states = bubble.selectAll('.state')
                     .data(data[0].values)
                     .enter().append('g')
                     .attr('class', 'state')
@@ -209,18 +193,18 @@ function initialize(error, data,first_time) {
         .attr('class', 'aggregate')
         .attr('cx', width/2)
         .attr('cy', height / 2)
-        .style('fill', function (d) { return color(d.key) })
+        .style('fill', function (d) {console.log(d.key, d.state_name,'state'); return color(d.key) })
         .style('opacity','.9')
         .on('click', function (d) { exploded.add(d.key); blurTransition.add(d.state_name) })
-        .append('title').text(function (d) { return d.key }).classed('bump_temp',true)
+        .append('title').text(function (d) { return d.key })
 
     update()
     d3.interval(incrementYear, interval)
     
     function incrementYear() {
-        if(year < 2017){ console.log(years.length, 'years length');
+        if(year < 2017){
         year = '' + years[++yearIndex >= years.length ? yearIndex = 0 : yearIndex]
-        console.log(year,'year');
+        
         update()
         
 
@@ -232,16 +216,12 @@ function initialize(error, data,first_time) {
 
         yearLabel.transition().duration(0).delay(interval/2).text(year)
     
-        var tooltip = d3.select("body").append("div")
-          .attr("class", "tooltip");
-
-       
 
         states.append('title').text(function (d) { return d.state_name })
 
         states = states.data(
             data.find(function (d) { return d.key === year }).values,
-            function (d) { console.log(d,"updating data for states"); return d.key }
+            function (d) { return d.key }
         )
 
         var counties = states.selectAll('.county')
@@ -272,7 +252,6 @@ function initialize(error, data,first_time) {
 
         states.select('.aggregate')
             .transition(t)
-
             .attr('r', function (d) { return
                 r(d.total_plan_selections);
             })
@@ -280,17 +259,6 @@ function initialize(error, data,first_time) {
             .attr('cy', function (d) { var retval = exploded.has(d.key) ? 0 : y(f(d.yes_aptc/d.total_plan_selections*100));
                 return retval
             })
-
-        // states.selectAll("text")
-        //   .on("mouseover", function(d) {
-        //     svg.selectAll('.state')
-        //         .classed('active', true);
-
-        //     var tooltip_str = "Yes APTC %: " + f(d.yes_aptc/d.total_plan_selections *100) +
-        //                     "<br/>" + "Region: " + d.state_name+
-        //             "<br/>" + "Total Plan Selection: " + d.total_plan_selections +
-        //             "<br/>" + "Year: " + d.year;})
-
 
         console.log("after selecting .aggregate");
 
@@ -304,19 +272,36 @@ function initialize(error, data,first_time) {
             .attr('cy', function (d) {
                 var retval = y(f((exploded.has(d.state_name) ? d : d.parent).yes_aptc/(exploded.has(d.state_name) ? d : d.parent).total_plan_selections *100));
                 console.log(retval);
-                if(retval){
-
-                    return retval}})
+                if(retval){ return retval;}})
                         
-        svg.selectAll("circle")
-          .on("mouseover", function(d) {
-            svg.selectAll('circle')
-                .classed('active', true);
+         // Tooltips
+          // var tooltip = d3.select("#bubble_chart").append("div")
+          //     .attr("class", "tooltip");
 
-            var tooltip_str = "Yes APTC %: " + f(d.yes_aptc/d.total_plan_selections *100) +
-                            "<br/>" + "Region: " + d.county_name+
-                    "<br/>" + "Total Plan Selection: " + d.total_plan_selections +
-                    "<br/>" + "Year: " + d.year;})
+          // bubble.selectAll("circle")
+          //     .on("mouseover", function(d) {
+          //       bubble.selectAll('.aggregate')
+          //           .classed('active', true);
+
+          //       var tooltip_str = "Yes APTC %: " + f(d.yes_aptc/d.total_plan_selections *100) +
+          //                   "<br/>" + "Region: " + d.state_name+
+          //           "<br/>" + "Total Plan Selection: " + d.total_plan_selections +
+          //           "<br/>" + "Year: " + d.year;
+
+          //       tooltip.html(tooltip_str)
+          //           .style("visibility", "visible");
+          //     })
+          //     .on("mousemove", function(d) {
+          //       tooltip.style("top", event.pageY - (tooltip.node().clientHeight + 5) + "px")
+          //           .style("left", event.pageX - (tooltip.node().clientWidth / 2.0) + "px");
+          //     })
+          //     .on("mouseout", function(d) {
+          //       bubble.selectAll('.aggregate')
+          //           .classed('active', false);
+
+          //       tooltip.style("visibility", "hidden");
+          //     })
+          
 
 
         blurValues
